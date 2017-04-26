923950bfb70272c5551d94d854ff7f428ce9f67d
majunhong <569847911@qq.com>
2017-04-26 20:34:51 +0800

    1

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ var xtpl = require('xtpl');
 var fs= require('fs');
 var multiparty = require('multiparty');
 var util = require('util');
+var bodyParser = require('body-parser');
+
 
 var app = express();
-var pid = 2; //全局产品id;
 
 //视图模版
 app.set('views', path.join(__dirname, 'views'));
@@ -16,7 +17,9 @@ app.set('view engine','xtpl');
 
 //使用静态资源
 app.use(express.static(path.join(__dirname, 'public')));
-
+//引用不然post获取不到参数
+app.use(bodyParser.urlencoded({ extended: false }))  
+app.use(bodyParser.json());
 /*app.get('/test',function(req,res){
 	res.render('test.xtpl',{});
 })*/
@@ -74,15 +77,19 @@ router.get('/detail/:id',function(req,res){
 //编辑产品
 router.get('/edit',function(req,res){
 	console.log('进入编辑');
-	res.render('edit',{},function(err,html){
+	var pid = global.pid || 1; //当前产品id
+	res.render('edit',{
+		pid:pid
+	},function(err,html){
 		res.send(html);
 	})
-})
+});
 
 //上传文件
 router.post('/uploadfile',function(req,res){
 	var form = new multiparty.Form({uploadDir:'./public/img/temp'});
 	var resFileUrlArry = [];
+	var pid = global.pid || 1; //全局产品id
 	form.parse(req,function(err,fields,files){
 		debugger;
 		var reFolder = 'public/img/pdetail/'+pid; //当前上传文件最终要存放的文件夹地址
@@ -103,6 +110,42 @@ router.post('/uploadfile',function(req,res){
 		console.log('输出的文件地址',resFileUrlArry);
 		res.json({path:resFileUrlArry});
 	})
+});
+
+//获取当前编辑的产品id对应的产品图片
+router.post('/editsave', function(req,res){
+	console.log('初始参数',req.body);
+	var params = JSON.parse(req.body.params);
+	console.log('传入的参数',params);
+	var pid = global.pid || 1;
+	var result = {};
+	try {
+		var nowProducts = JSON.parse(fs.readFileSync('./jsonfile/products.json'));
+		var nowItemList = nowProducts[params.itemType] || []; //当前产品类别数据列表
+		nowItemList.push({
+			"id":pid,
+			"url":params.url,
+			"name":params.name,
+			"price":params.price
+		}); //插入当前保存的数据
+		console.log('当前产品类别的数据列表',nowItemList);
+		nowProducts[params.itemType] = nowItemList;
+		var nowProductDetails = JSON.parse(fs.readFileSync('./jsonfile/productDetails.json')); //当前详情数据列表
+		nowProductDetails[pid] = {
+			"imgList":params.imgList,
+			"attribute":params.attribute,
+			"description":params.description
+		}
+		console.log('当前产品详情数据列表',nowProductDetails);
+		//写入文件
+		fs.writeFileSync('./jsonfile/products.json',JSON.stringify(nowProducts));
+		fs.writeFileSync('./jsonfile/productDetails.json',JSON.stringify(nowProductDetails));
+		global.pid = pid + 1;
+		result = {msg:'保存成功'};
+	}catch(err){
+		result = {msg:'保存失败',err:err};
+	}
+	res.json(result);
 })
 
 app.use('/',router);
